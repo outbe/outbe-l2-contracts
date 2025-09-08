5cfb865b0dfd4413ee5693ff3d33c675e410eab0
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-09-08 15:44:25 +0100

    Revert "chore: refactor interfaces"
    This reverts commit be11980f485801a2c2f6d887814a84b0e8f19244.

--- src/cra_registry/CRARegistryUpgradeable.sol
+++ src/cra_registry/CRARegistryUpgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ICRARegistry} from "./ICRARegistry.sol";
+import {ICRARegistry} from "../interfaces/ICRARegistry.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
--- test/cra_registry/CRARegistryUpgradeable.t.sol
+++ test/cra_registry/CRARegistryUpgradeable.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {CRARegistryUpgradeable} from "../../src/cra_registry/CRARegistryUpgradeable.sol";
-import {ICRARegistry} from "../../src/cra_registry/ICRARegistry.sol";
+import {ICRARegistry} from "../../src/interfaces/ICRARegistry.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract CRARegistryUpgradeableTest is Test {
