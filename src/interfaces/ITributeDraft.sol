cc224d5eabf5e0bf764b5f14877246ab16429cbb
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-11-05 17:02:44 +0000

    add hashes

--- src/interfaces/ITributeDraft.sol
+++ src/interfaces/ITributeDraft.sol
@@ -13,7 +13,8 @@ interface ITributeDraft is ISoulBoundToken {
         uint32 worldwideDay,
         uint64 settlementAmountBase,
         uint128 settlementAmountAtto,
-        uint16 settlementCurrency
+        uint16 settlementCurrency,
+        uint256[] cuHashes
     );
 
     struct TributeDraftEntity {
@@ -59,7 +60,7 @@ interface ITributeDraft is ISoulBoundToken {
     /// @param indexTo inclusive index to
     /// @return array with complete tribute draft data
     function getTributeDraftsByOwner(address owner, uint256 indexFrom, uint256 indexTo)
-        external
-        view
-        returns (TributeDraftEntity[] memory);
+    external
+    view
+    returns (TributeDraftEntity[] memory);
 }
